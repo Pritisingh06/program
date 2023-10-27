02a397bb72b82af9fe8fb74f720d446e2f1d4a80
Pritisingh06 <142310439+Pritisingh06@users.noreply.github.com>
2023-10-27 17:51:15 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"Enter first no. :";
+cin>>a;
+cout<<"Enter second no.:";
+cin>>b;
+if(a>b){
+cout<<"hello";
+}
+else{
+cout<<"good morning";
+}
+return 0;
+}
